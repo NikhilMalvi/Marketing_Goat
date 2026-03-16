da1bccaa84939cdd04706f00f97921ac880e2b8a
Your Name <your-email@example.com>
2026-03-16 12:59:52 +0530

    add home tab

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,12 +4,14 @@ import OverlayBackground from "../components/OverlayBackground";
 import Counter from "../components/CustomCounter";
 import HomeHero from "../components/home/HomeHero";
 import SectionCompoment from "../components/home/SectionCompoment";
+import HomeTab from "../components/home/HomeTab";
 
 const Home = () => {
   return (
     <>
       <HomeHero />
       <SectionCompoment />
+      <HomeTab />
     </>
   );
 };
--- src/components/home/HomeTab.jsx
+++ src/components/home/HomeTab.jsx
@@ -2,7 +2,17 @@ import React from "react";
 
 const HomeTab = () => {
   return (
-    <section className="w-full px-3.75 lg:py-25 py-15 relative overflow-hidden"></section>
+    <section className="w-full px-3.75 lg:py-25 py-15 relative overflow-hidden">
+      <div className="max-w-375">
+        <h2 className="primary-heading">How We Build Successful Website</h2>
+        <p className="peragraph text-paragraph">
+          We provide specialized services in design, development, and
+          marketing—dedicated to creating powerful digital experiences and
+          driving tangible business success.
+        </p>
+        <div className=""></div>
+      </div>
+    </section>
   );
 };
 
