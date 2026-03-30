c92a107b1758dbd2fad1c87ffd2cb7c122d5b7f9
Your Name <your-email@example.com>
2026-03-30 10:44:52 +0530

    Line 17 of Contact.jsx: Update textClass to include leading for better spacing.

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
         <div className="max-w-375 mx-auto">
           <Form
             text="Get in Touch & Build together with It Solutions"
-            textClass="text-black text-[30px]! "
+            textClass="text-black text-[30px]! leading-[40px]!"
             inputclass="bg-[#4C95FF1A] text-[#00000080] placeholder:text-[#00000080]"
             labelclass="text-paragraph-2"
             checkboxclass="border border-[#DCDCDC] rounded-[60px] text-[#808080]"
